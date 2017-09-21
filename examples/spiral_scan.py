ef15cbe77bc2fc0c2df55fafff13ee74f1bc7a7f
Eva <eva@irnas.eu>
2017-09-21 12:37:25 +0200

    Update backlash 120

--- examples/spiral_scan.py
+++ examples/spiral_scan.py
@@ -141,7 +141,7 @@ class KoruzaAPI(object):
 # Tracking class
 class Spiral_scan(object):
     N_CIRCLE = 100
-    BACKLASH = 110  # Backlash
+    BACKLASH = 120  # Backlash
     STEP = 50
 
     def __init__(self):
