f6d55edfcc4ee2e7b2f22db286ec5aa5c2ed4ba4
Eva <eva@irnas.eu>
2018-01-26 14:33:38 +0100

    Step size 20/50

--- examples/spiral_scan.py
+++ examples/spiral_scan.py
@@ -140,9 +140,9 @@ class KoruzaAPI(object):
 
 # Tracking class
 class Spiral_scan(object):
-    N_CIRCLE = 10
+    N_CIRCLE = 20
     BACKLASH = 120  # Backlash
-    STEP = 100
+    STEP = 50
 
     def __init__(self):
         """Initialise all variables"""
