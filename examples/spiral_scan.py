be6bf8b99f3bc9b07da782146ecc121cf752fb49
Eva <eva@irnas.eu>
2018-01-30 08:50:10 +0100

    step 15/50

--- examples/spiral_scan.py
+++ examples/spiral_scan.py
@@ -142,7 +142,7 @@ class KoruzaAPI(object):
 class Spiral_scan(object):
     N_CIRCLE = 15
     BACKLASH = 120  # Backlash
-    STEP = 100
+    STEP = 50
 
     def __init__(self):
         """Initialise all variables"""
